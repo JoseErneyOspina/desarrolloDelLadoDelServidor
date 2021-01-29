0f38cbc5c5d3b0bcdd0f38e8614aa2721cfa2e6d
JoseErneyOspina <joseerneyosp@gmail.com>
2021-01-29 01:44:39 -0500

    odm utilizando mongoose en nuestro proyecto

--- project/red-bicicletas/models/bicicleta.js
+++ project/red-bicicletas/models/bicicleta.js
@@ -1,45 +1,51 @@
-var Bicicleta = function (id, color, modelo, ubicacion) {
-    this.id = id;
-    this.color = color;
-    this.modelo = modelo;
-    this.ubicacion = ubicacion;
-}
+// Uso de mongoose y schemas
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-Bicicleta.prototype.toString = function (){
-    return 'id: ' + this.id + " | color: " + this.color;
-}
-
-Bicicleta.allBicis = [];
-Bicicleta.add = function(aBici){
-    Bicicleta.allBicis.push(aBici);
-}
-
-
-// Para hacer una busqueda usamos el metodo find by ID
-Bicicleta.findById = function(aBiciId){
-    var aBici = Bicicleta.allBicis.find(x => x.id ==aBiciId);
-    if (aBici)
-        return aBici;
-    else
-        throw new Error(`No existe una bicicleta con el id ${aBiciId}`);
-}
-
-// Metodo de remove byId
-Bicicleta.removeById = function(aBiciId){
-    for(var i = 0; i < Bicicleta.allBicis.length; i++){
-        if (Bicicleta.allBicis[i].id == aBiciId){
-            Bicicleta.allBicis.splice(i, 1);
-            break;
-        }
+// Definimos el esquema
+var bicicletaSchema = new Schema ({
+    code: Number,
+    color: String,
+    modelo: String,
+    ubicacion: {
+        type: [Number], index: { type: '2dsphere', sparse: true}
     }
-}
+});
 
-/*
-var a = new Bicicleta(1, 'rojo', 'urbana', [-34.6012424,-58.3861497]);
-var b = new Bicicleta(2, 'blanca', 'urbana', [-34.596932,-58.3808287]);
+// 
+bicicletaSchema.statics.createInstance = function(code, color, modelo, ubicacion){
+    return new this({
+        code: code,
+        color: color,
+        modelo: modelo,
+        unicacion: ubicacion
+    });
+};
 
-Bicicleta.add(a);
-Bicicleta.add(b);
-*/
+// Definimos una instancia con mongoose
+bicicletaSchema.methods.toString = function() {
+    return 'code: ' + this.code + ' | color: ' + this.color;
+};
 
-module.exports = Bicicleta;
+// Todas las bicicletas
+bicicletaSchema.statics.allBicis = function(cb){
+    return this.find({}, cb);
+};
+
+// Create
+bicicletaSchema.statics.add = function(aBici, cb){
+    this.create(aBici, cb);
+};
+
+// find byCode
+bicicletaSchema.statics.findByCode = function(aCode, cb){
+    return this.findOne({code: aCode}, cb);
+};
+
+// remove ByCode
+bicicletaSchema.statics.removeByCode = function(aCode, cb){
+    return this.deleteOne({code: aCode}, cb);
+};
+
+// Exportamos el modelo
+module.exports = mongoose.model('Bicicleta', bicicletaSchema);
