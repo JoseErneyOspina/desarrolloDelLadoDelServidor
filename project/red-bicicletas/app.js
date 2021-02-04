4165b0a5d7df640474004635fb3b845f177dfcea
JoseErneyOspina <joseerneyosp@gmail.com>
2021-02-03 23:57:10 -0500

    sesiones y cookies

--- project/red-bicicletas/app.js
+++ project/red-bicicletas/app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// Incluimos
+const passport = require('./config/passport');
+// Creamos el objeto session
+const session = require('express-session');
 
 var indexRouter = require('./routes/index');
 // Agregamos usuarios
@@ -14,7 +18,19 @@ var bicicletasRouter = require('./routes/bicicletas');
 var bicicletasAPIRouter = require('./routes/api/bicicletas');
 var usuarioAPIRouter = require('./routes/api/usuarios');
 
+// Definimos un objeto store para definir cual es motor de session que vamos a usar
+// Utilizamos el MemoryStore sera bastante agil ya que quedara en la memoria del servidor
+const store = session.MemoryStore;
+
 var app = express();
+// Lo que hacemos una vez definido el express es decirle a app que use session
+app.use(session({
+  cookie: { mazAge: 240 * 60 * 60 * 1000 },
+  sote: store,
+  saveUninitialized: true,
+  resave: 'true',
+  secret: 'red_bicis_!!!***!".!".!".!".123123'
+}));
 
 // Referenciamos a moongoose
 var mongoose = require('mongoose');
@@ -33,6 +49,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+// Despues del cookieParser también inicializar passport y passport.session
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
