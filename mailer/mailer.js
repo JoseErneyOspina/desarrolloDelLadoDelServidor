ec06d30fe72a96f814f9e1cb24e6aa589fe55456
JoseErneyOspina <joseerneyosp@gmail.com>
2021-02-06 20:19:32 -0500

    agregamos sendgrid

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,12 +1,44 @@
 const nodemailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
 
-const mailConfig = {
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-        user: 'dszfks4wymsjsnvio@ethereal.email',
-        pass: 'p9xgyUfPqK9yHytusn'
+let mailConfig;
+
+if(process.env.NODE_ENV === 'production') {
+    const options = {
+        auth: {
+            api_key: process.env.SENDGRID_API_SECRET
+        }
     }
-};
+    mailConfig = sgTransport(options);
+} else {
+    if (process.env.NODE_ENV === 'staging') {
+        console.log('XXXXXXXXXXXXXXXXXXXXXX');
+        const options = {
+            auth: {
+                api_key: process.env.SENDGRID_API_SECRET
+            }
+        }
+        mailConfig = sgTransport(options);
+    } else {
+        // all emails are catched by ethereal.email
+        mailConfig = {
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: process.env.ethereal_user,
+                pass: process.env.ethereal_pwd
+            }
+        }
+    }
+}
+
+// const mailConfig = {
+//     host: 'smtp.ethereal.email',
+//     port: 587,
+//     auth: {
+//         user: 'dszfks4wymsjsnvio@ethereal.email',
+//         pass: 'p9xgyUfPqK9yHytusn'
+//     }
+// };
 
 module.exports = nodemailer.createTransport(mailConfig);
